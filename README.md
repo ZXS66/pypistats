f701a3006299054f37d0fd39ef4f46faf4659e9e
ZXS <zxs.dev@outlook.com>
2025-11-10 14:32:32 +0800

    ver 1.0.1: update backend API endpoint

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-This is a chromium extension to help you find downloads amount for specific python packages. For more info visit: [Microsoft Edge Addons](https://microsoftedge.microsoft.com/addons/detail/pypi-downloads-stats/dncpkpafbfaechcdhfhgoebfdfccogij)
+This is a [Microsoft Edge extension](https://microsoftedge.microsoft.com/addons/detail/pypi-downloads-stats/dncpkpafbfaechcdhfhgoebfdfccogij) to help you find downloads amount for python packages [checkout "meta" section under left sidebar], which is useful for python developer to identify if current package is popular or not.
 
-It is rely on https://pypistats.org/, which query Google Cloud BigQuery to get the data.
+![sample screenshot](./images/screenshot.png)
 
-Due to CORS restriction, this extension can only query the data from a proxy which enable CORS, thus you may [build your own proxy server](https://github.com/ZXS66/pypistats-proxy) if you want to use it, or you can use this extension for free (no ads) on Microsoft Edge. (personal use only, be nice.)
+It is rely on [pypistats.org](https://pypistats.org/), which querying Google Cloud BigQuery to get the data.
+
+Due to CORS restriction, this extension can only query the data from a proxy which enable CORS, thus you may [build your own proxy server](https://github.com/ZXS66/pypistats-proxy) or use third party CORS proxy(such as [CorsProxy.io](https://corsproxy.io) or [crossorigin.me](https://corsproxy.github.io/)) if you want to use it , or you can use this extension for free (no ads) on Microsoft Edge (personal use only, be nice).
--- scripts/init.js
+++ scripts/init.js
@@ -1,82 +1,85 @@
 /** meta data for the PyPi package */
 class DownloadStatsMeta {
-    /**
-     * initialize the meta data
-     * @param {string} id python package id
-     * @param {number} downloads_lastday 
-     * @param {number} downloads_lastweek 
-     * @param {number} downloads_lastmonth 
-     */
-    constructor(id, downloads_lastday, downloads_lastweek, downloads_lastmonth) {
-        this.id = id;
-        this.downloads_lastday = downloads_lastday;
-        this.downloads_lastweek = downloads_lastweek;
-        this.downloads_lastmonth = downloads_lastmonth;
-    }
+  /**
+   * initialize the meta data
+   * @param {string} id python package id
+   * @param {number} downloads_lastday
+   * @param {number} downloads_lastweek
+   * @param {number} downloads_lastmonth
+   */
+  constructor(id, downloads_lastday, downloads_lastweek, downloads_lastmonth) {
+    this.id = id;
+    this.downloads_lastday = downloads_lastday;
+    this.downloads_lastweek = downloads_lastweek;
+    this.downloads_lastmonth = downloads_lastmonth;
+  }
 }
 
 /** the name of this extension */
 const _EXTENSION_NAME = "pypistats";
 
 /** base URL of backend API */
-const API_BASE = "https://johnzhu.online/pypistats";
+const API_BASE = "https://johnzhu.cn/pypistats";
 
 /**
  * render the StatsMeta into page
- * @param {DownloadStatsMeta} data 
+ * @param {DownloadStatsMeta} data
  */
 function renderMetadata(data) {
-    if (!data) {
-        console.error(`[${_EXTENSION_NAME}] invalid data received`);
-        return;
+  if (!data) {
+    console.error(`[${_EXTENSION_NAME}] invalid data received`);
+    return;
+  }
+  const headers = document.querySelectorAll(".sidebar-section h6");
+  let metaHeader = null;
+  // find the meta header
+  for (let i = 0; i < headers.length; i++) {
+    if (headers[i].textContent.trim() === "Meta") {
+      metaHeader = headers[i];
+      break;
     }
-    const headers = document.querySelectorAll('.sidebar-section h6');
-    let metaHeader = null;
-    // find the meta header
-    for (let i = 0; i < headers.length; i++) {
-        if (headers[i].textContent.trim() === "Meta") {
-            metaHeader = headers[i];
-            break;
-        }
-    }
-    // find the ul element by metaHeader
-    const ul = metaHeader?.nextElementSibling;
-    if (!ul?.tagName === 'UL') {
-        console.error(`[${_EXTENSION_NAME}] cannot find ul element`);
-        return;
-    }
-    // append downloads info
-    const appendItem = (key, value) => {
-        const li = document.createElement('li');
-        li.innerHTML = `<span><strong>${key}:</strong> ${value}</span>`;
-        ul.appendChild(li);
-    };
-    appendItem('Downloads last day', formatNumber(data.downloads_lastday));
-    appendItem('Downloads last week', formatNumber(data.downloads_lastweek));
-    appendItem('Downloads last month', formatNumber(data.downloads_lastmonth));
+  }
+  // find the ul element by metaHeader
+  const ul = metaHeader?.nextElementSibling;
+  if (!ul?.tagName === "UL") {
+    console.error(`[${_EXTENSION_NAME}] cannot find ul element`);
+    return;
+  }
+  // append downloads info
+  const appendItem = (key, value) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<span><strong>${key}:</strong> ${value}</span>`;
+    ul.appendChild(li);
+  };
+  appendItem("Downloads last day", formatNumber(data.downloads_lastday));
+  appendItem("Downloads last week", formatNumber(data.downloads_lastweek));
+  appendItem("Downloads last month", formatNumber(data.downloads_lastmonth));
 }
 
 function formatNumber(num) {
-    num = +num;
-    return num.toLocaleString('en-US');
+  num = +num;
+  return num.toLocaleString("en-US");
 }
 
 /**
  * get package meta data by package id
  * @param {string} packageId
- * @returns {Promise<DownloadStatsMeta>} 
+ * @returns {Promise<DownloadStatsMeta>}
  */
 const queryDownloadStatsMeta = async (packageId) => {
-    const url = `${API_BASE}/package/${packageId}`;
-    const resp = await fetch(url, { method: 'POST' });
-    return await resp.json();
-}
+  const url = `${API_BASE}/package/${packageId}`;
+  const resp = await fetch(url, { method: "POST" });
+  return await resp.json();
+};
 
 // initialization: check if the current page is the package detail page
-if (window.location.hostname === 'pypi.org' && window.location.pathname.startsWith('/project/')) {
-    document.addEventListener("DOMContentLoaded", async () => {
-        const packageId = window.location.pathname.split('/')[2];
-        const data = await queryDownloadStatsMeta(packageId);
-        renderMetadata(data);
-    });
+if (
+  window.location.hostname === "pypi.org" &&
+  window.location.pathname.startsWith("/project/")
+) {
+  document.addEventListener("DOMContentLoaded", async () => {
+    const packageId = window.location.pathname.split("/")[2];
+    const data = await queryDownloadStatsMeta(packageId);
+    renderMetadata(data);
+  });
 }
