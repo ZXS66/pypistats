49ca08b10c9bb13b971d8b99564b284b3bce157b
ZXS66 <zxs.dev@outlook.com>
2025-11-22 10:58:01 +0800

    1. show loading status  2. replace backend API endpoint with public CORS proxy

--- scripts/init.js
+++ scripts/init.js
@@ -18,18 +18,12 @@ class DownloadStatsMeta {
 /** the name of this extension */
 const _EXTENSION_NAME = "pypistats";
 
-/** base URL of backend API */
-const API_BASE = "https://johnzhu.cn/pypistats";
 
-/**
- * render the StatsMeta into page
- * @param {DownloadStatsMeta} data
+/** 
+ * get the html container, which to be appended download data 
+ * @returns {HTMLElement} the ul element under Meta section
  */
-function renderMetadata(data) {
-  if (!data) {
-    console.error(`[${_EXTENSION_NAME}] invalid data received`);
-    return;
-  }
+function getMetaContainer() {
   const headers = document.querySelectorAll(".sidebar-section h6");
   let metaHeader = null;
   // find the meta header
@@ -45,20 +39,51 @@ function renderMetadata(data) {
     console.error(`[${_EXTENSION_NAME}] cannot find ul element`);
     return;
   }
+  return ul;
+}
+
+
+/** render loading state */
+function renderLoading() {
+  const container = getMetaContainer();
+  if (!container) {
+    return;
+  }
+  const li = document.createElement("li");
+  li.id = "pypistats-loading";
+  li.innerHTML = `<span><strong>[${_EXTENSION_NAME}]</strong> loading...</span>`;
+  container.appendChild(li);
+  return li;
+}
+
+/**
+ * render the StatsMeta into page
+ * @param {DownloadStatsMeta} data
+ */
+function renderMetadata(data) {
+  if (!data) {
+    console.error(`[${_EXTENSION_NAME}] invalid data received`);
+    return;
+  }
   // append downloads info
   const appendItem = (key, value) => {
     const li = document.createElement("li");
     li.innerHTML = `<span><strong>${key}:</strong> ${value}</span>`;
-    ul.appendChild(li);
+    container.appendChild(li);
   };
-  appendItem("Downloads last day", formatNumber(data.downloads_lastday));
-  appendItem("Downloads last week", formatNumber(data.downloads_lastweek));
-  appendItem("Downloads last month", formatNumber(data.downloads_lastmonth));
-}
-
-function formatNumber(num) {
-  num = +num;
-  return num.toLocaleString("en-US");
+  const formatNumber = (num) => {
+    num = +num;
+    return num.toLocaleString("en-US");
+  }
+  // sample data: {"data":{"last_day":10720429,"last_month":259462935,"last_week":62910252},"package":"python-dotenv","type":"recent_downloads"}
+  const container = getMetaContainer();
+  if (!container) {
+    return;
+  }
+  const downloads = data.data;
+  appendItem("Downloads last day", formatNumber(downloads?.last_day));
+  appendItem("Downloads last week", formatNumber(downloads?.last_week));
+  appendItem("Downloads last month", formatNumber(downloads?.last_month));
 }
 
 /**
@@ -67,9 +92,10 @@ function formatNumber(num) {
  * @returns {Promise<DownloadStatsMeta>}
  */
 const queryDownloadStatsMeta = async (packageId) => {
-  const url = `${API_BASE}/package/${packageId}`;
-  const resp = await fetch(url, { method: "POST" });
-  return await resp.json();
+  const url = `https://corsproxy.io/https://pypistats.org/api/packages/${packageId}/recent`;
+  const resp = await fetch(url);
+  const data = await resp.json();
+  return data;
 };
 
 // initialization: check if the current page is the package detail page
@@ -78,8 +104,10 @@ if (
   window.location.pathname.startsWith("/project/")
 ) {
   document.addEventListener("DOMContentLoaded", async () => {
+    const loadingElement = renderLoading();
     const packageId = window.location.pathname.split("/")[2];
     const data = await queryDownloadStatsMeta(packageId);
+    loadingElement?.remove();
     renderMetadata(data);
   });
 }
